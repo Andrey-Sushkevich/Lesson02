ffb82a689983e4efacc4b60f143c003b23699561
Andrey-Sushkevich <64629724+Andrey-Sushkevich@users.noreply.github.com>
2020-06-23 19:14:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+/*Задание 0*/
+let str = "Привет";
+alert(typeof str);
+let numb = 123;
+alert(typeof numb);
+let flag = true;
+alert(typeof flag);
+let txt = "true";
+alert(typeof txt);
+
+/*Задание 1*/
+
+    let a1= (5 + 3)*(5 - 3);
+    let a2= (10 - 6)*200;
+    let a3= 534*132/18
+    let a4= 123*3 + 10;
+    let a5= 12*13*14;
+    alert([a1, a2, a3, a4, a5]); 
+
+/*Задание 2*/
+
+    let h =43 ;
+        w=27;
+        s=h*w;
+    alert(s);
+
+  /*Задание 3*/
+   
+    let n=3;
+        m=4;
+        k=m*m + n*n;
+    alert(Math.sqrt(k));
+
+    /*Задание 4*/
+    let sum=1000;
+        rate=10;
+        years=5;
+        payment=sum/100*rate*years;
+    alert(payment);
+
+    /*Задание 5*/
+    let minute = 60;
+        hour = minute * 60;
+        day = hour * 24;
+        week = day * 7;
+        phrase="Сегодня, 22 июня, я узнал что количество секунд в одной неделе составляет ";
+    alert(phrase + week);
+        
+    /*Задание 6*/
+    
+   let a=10;
+       b=2;
+       c=5;
+   alert(a + b + c);
+
+    /*Задание 7*/
+    let age = 31;
+    alert('Мне ' + age + ' год!');
+
+    /*Задание 8*/
+    let clock = 1;
+        minutes = 5;
+        seconds = 28;
+    alert('часы: ' + clock + ' минуты: ' + minutes + ' секунд: ' + seconds);
+
+    /*Задание 9*/
+    let num = 10;
+        num = ++num;
+        num = ++num;
+        num = --num;
+    alert(num);
+
+/*Задание 10*/
+
+    let a6 = 5 % 3,
+        a7 = 3 % 5,
+        a8 = 5 + '3',
+        a9 = '5' - 3,
+        a10 = 75 + 'кг';
+        
+    alert([a6, a7, a8, a9, a10]); 
